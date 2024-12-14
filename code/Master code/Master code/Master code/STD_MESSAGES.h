59acdcdcbb68cae9ed1364b0f526ef9280a8668b
Bishoy Attia Roshdy <74090976+Bishoy4@users.noreply.github.com>
2024-12-14 14:49:13 +0200

    Add files via upload

---
+++ code/Master code/Master code/Master code/STD_MESSAGES.h
@@ -0,0 +1,21 @@
+#ifndef STD_MESSAGES_H_
+#define STD_MESSAGES_H_
+
+#define SERVO_STATUS    0x11
+#define LDR_STATUS    0x12
+
+#define SERVO_TURN_ON    0x21
+#define LDR_TURN_ON    0x22
+
+#define SERVO_TURN_OFF    0x31
+#define LDR_TURN_OFF    0x32
+
+#define SET_TEMPERATURE 0x40
+
+#define DEFAULT_ACK    0xFF
+#define DEMAND_RESPONSE 0xFF
+
+#define ON_STATUS   0x01
+#define OFF_STATUS  0x00
+
+#endif
